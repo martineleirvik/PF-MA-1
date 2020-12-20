46cfb9d5bdf1f37c3db5a0ba7ec728f2d3246988
martineleirvik <leirvikmartine@gmail.com>
2020-12-20 15:57:47 +0100

    new

---
+++ js/script1.js
@@ -0,0 +1,46 @@
+console.log("This is script 1");
+
+
+// question 1
+
+var name = "John" ;
+
+var price = 5 
+
+var loggedIn = false
+
+
+// question 2
+
+var firstName = "Martine" ;
+var lastName = "Leirvik" ;
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+// question 3
+
+var animal = "frog"
+var typeOfAnimal = typeof(animal);
+console.log(typeOfAnimal);
+//string
+
+
+// question 4
+
+var orderHasShipped = true;
+
+if(orderHasShipped === true) {
+    console.log(orderHasShipped);
+} else {
+    console.log("The order did not ship")
+}
+
+
+//question 5
+
+for (var i = 7; i <= 13; i++) {
+    console.log(i);
+}
